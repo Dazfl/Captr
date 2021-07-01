8fa887083e31a7b5d901332e9eacb8d2ac9adb98
Darryn Flood <darryn@pennie.com.au>
2021-07-01 17:44:25 +1000

    Added some unit tests

---
+++ Sample/SimpleBank.UnitTests/DomainTests/AggregateTests/AccountTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using SimpleBank.Domain.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleBank.UnitTests.DomainTests.AggregateTests
+{
+	public class AccountTests
+	{
+		[Fact]
+		public void Account_Create_ShouldSucceed()
+		{
+			// Arrange
+			string accountNumber = "123456789";
+			long openingBalance = 1000;
+			bool isActive = true;
+
+			// Act
+			Account account = new(accountNumber, openingBalance, isActive);
+
+			// Assert
+			account.Should().NotBeNull();
+			account.AccountNumber.Should().Be(accountNumber);
+			account.Balance.Should().Be(openingBalance);
+			account.IsActive.Should().Be(isActive);
+			account.GetAggregateId().Should().Be(accountNumber);
+			account.GetChanges().Count.Should().Be(1);
+		}
+
+		[Fact]
+		public void Account_Deposit_ShouldSucceed()
+		{
+			// Arrange
+			string accountNumber = "123456789";
+			long openingBalance = 1000;
+			long deposit = 500;
+			bool isActive = true;
+
+			// Act
+			Account account = new(accountNumber, openingBalance, isActive);
+			account.Deposit(deposit);
+
+			// Assert
+			account.Balance.Should().Be(openingBalance + deposit);
+			account.GetChanges().Count.Should().Be(2);
+		}
+
+		[Fact]
+		public void Account_Withdraw_ShouldSucceed()
+		{
+			// Arrange
+			string accountNumber = "123456789";
+			long openingBalance = 1000;
+			long withdraw = 450;
+			bool isActive = true;
+
+			// Act
+			Account account = new(accountNumber, openingBalance, isActive);
+			account.Withdraw(withdraw);
+
+			// Assert
+			account.Balance.Should().Be(openingBalance - withdraw);
+			account.GetChanges().Count.Should().Be(2);
+		}
+	}
+}
---
+++ Sample/SimpleBank.UnitTests/FeatureTests/AccountsTests/CommandsTests/CreateAccountTests/CreateAccountTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using SimpleBank.Domain.Aggregates;
+using SimpleBank.Features.Accounts.Commands.CreateAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleBank.UnitTests.FeatureTests.AccountsTests.CommandsTests.CreateAccountTests
+{
+	public class CreateAccountTests
+	{
+		[Fact]
+		public async Task CreateAccount_ShouldSucceed()
+		{
+			// Arrange
+			string accountNumber = "123456789";
+			long openingBalance = 1000;
+			bool isActive = true;
+			var command = new CreateAccount.Command(accountNumber, openingBalance, isActive);
+			Account account = null;
+			CancellationToken token = CancellationToken.None;
+
+			// Act
+			var commandHandler = new CreateAccount.CommandHandler((x, token) => Task.FromResult(account), (x, token) => Task.FromResult(true));
+			var result = await commandHandler.Handle(command, token);
+
+			// Assert
+			result.IsSuccessful.Should().BeTrue();
+			result.Message.Should().BeNullOrEmpty();
+		}
+	}
+}
