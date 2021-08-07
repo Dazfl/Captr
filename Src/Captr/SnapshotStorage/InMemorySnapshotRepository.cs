a39a1ee3d1dcf6f653eced6cafd9561c39a72d71
Dazfl <30303952+Dazfl@users.noreply.github.com>
2021-08-07 19:31:18 +1000

    Added in-memory storage option for testing purposes (#2)
    Co-authored-by: Darryn Flood <darryn@pennie.com.au>

---
+++ Src/Captr/SnapshotStorage/InMemorySnapshotRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Captr.SnapshotStorage
+{
+	public class InMemorySnapshotRepository : ISnapshotRepository
+	{
+		public Task<SnapshotDescriptor> GetSnapshotAsync(string streamId, CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> SaveSnapshotAsync(SnapshotDescriptor snapshotDescriptor, CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
